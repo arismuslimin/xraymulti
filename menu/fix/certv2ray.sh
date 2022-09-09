7d4f0a73b5a1dc4ead69d56760683df75679c29c
arismuslimin <93474156+arismuslimin@users.noreply.github.com>
2022-09-09 07:31:59 +0700

    Update certv2ray.sh

--- menu/fix/certv2ray.sh
+++ menu/fix/certv2ray.sh
@@ -18,12 +18,12 @@ source /var/lib/crot/ipvps.conf
 domain=$(cat /etc/xray/domain)
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 cd /root/
-wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+wget -O -  https://get.acme.sh | sh -s email=arismuslimin@gmail.com
 bash acme.sh --install
 rm acme.sh
 cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
-bash acme.sh --register-account -m kimochilol@gmail.com
+bash acme.sh --register-account -m arismuslimin@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
 cd
